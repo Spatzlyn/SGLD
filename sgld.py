abfa1ed4476a39db5e20ddfcd3c1293006b347a8
Spatzlyn <spatzlyn.yk@gmail.com>
2025-03-26 21:30:03 +0900

    0326_21:30:

--- sgld.py
+++ sgld.py
@@ -2,3 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from scipy.stats import multivariate_normal
+
+#N(mu,cov)-> target distribution
+mu=np.array([1.0,1.0])
+cov=np.array([[0.5,0.0],[0.0,0.5]])
+cov_inverse=np.linalg.inv(cov)
+
+#logprob: log probability function, gradlog: gradient of log probability
+def logprob(theta):
+    diff=theta-mu
+    return -0.5*diff.T @ cov_inverse @ diff
+
+def gradlog(theta):
+    return -cov_inverse@(theta-mu)
